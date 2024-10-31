ee31a0cb27eb95e0dc08dd5251f1bb4d66c8a6a4
PhamTuanVu31032003 <ptvu310303@gmail.com>
2024-10-31 21:01:39 +0700

    form
    form

---
+++ QuanLyThuVien/QuanLyThuVien/FormQLThuVien.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace QuanLyThuVien
+{
+    partial class FormQLThuVien
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.hệThốngToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.quảnLýĐộcGiảToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.quảnLýSáchToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.quảnLýTácGiảToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.quảnLýMượnTrảToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.quảnLýMượnToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.quảnLýTrảToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.hệThốngToolStripMenuItem,
+            this.quảnLýĐộcGiảToolStripMenuItem,
+            this.quảnLýSáchToolStripMenuItem,
+            this.quảnLýTácGiảToolStripMenuItem,
+            this.quảnLýMượnTrảToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(953, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // hệThốngToolStripMenuItem
+            // 
+            this.hệThốngToolStripMenuItem.Name = "hệThốngToolStripMenuItem";
+            this.hệThốngToolStripMenuItem.Size = new System.Drawing.Size(88, 24);
+            this.hệThốngToolStripMenuItem.Text = "Hệ Thống";
+            this.hệThốngToolStripMenuItem.Click += new System.EventHandler(this.hệThốngToolStripMenuItem_Click);
+            // 
+            // quảnLýĐộcGiảToolStripMenuItem
+            // 
+            this.quảnLýĐộcGiảToolStripMenuItem.Name = "quảnLýĐộcGiảToolStripMenuItem";
+            this.quảnLýĐộcGiảToolStripMenuItem.Size = new System.Drawing.Size(132, 24);
+            this.quảnLýĐộcGiảToolStripMenuItem.Text = "Quản Lý Độc Giả";
+            this.quảnLýĐộcGiảToolStripMenuItem.Click += new System.EventHandler(this.quảnLýĐộcGiảToolStripMenuItem_Click);
+            // 
+            // quảnLýSáchToolStripMenuItem
+            // 
+            this.quảnLýSáchToolStripMenuItem.Name = "quảnLýSáchToolStripMenuItem";
+            this.quảnLýSáchToolStripMenuItem.Size = new System.Drawing.Size(110, 24);
+            this.quảnLýSáchToolStripMenuItem.Text = "Quản Lý Sách";
+            this.quảnLýSáchToolStripMenuItem.Click += new System.EventHandler(this.quảnLýSáchToolStripMenuItem_Click);
+            // 
+            // quảnLýTácGiảToolStripMenuItem
+            // 
+            this.quảnLýTácGiảToolStripMenuItem.Name = "quảnLýTácGiảToolStripMenuItem";
+            this.quảnLýTácGiảToolStripMenuItem.Size = new System.Drawing.Size(126, 24);
+            this.quảnLýTácGiảToolStripMenuItem.Text = "Quản Lý Tác Giả";
+            this.quảnLýTácGiảToolStripMenuItem.Click += new System.EventHandler(this.quảnLýTácGiảToolStripMenuItem_Click);
+            // 
+            // quảnLýMượnTrảToolStripMenuItem
+            // 
+            this.quảnLýMượnTrảToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.quảnLýMượnToolStripMenuItem,
+            this.quảnLýTrảToolStripMenuItem});
+            this.quảnLýMượnTrảToolStripMenuItem.Name = "quảnLýMượnTrảToolStripMenuItem";
+            this.quảnLýMượnTrảToolStripMenuItem.Size = new System.Drawing.Size(142, 24);
+            this.quảnLýMượnTrảToolStripMenuItem.Text = "Quản Lý Mượn Trả";
+            // 
+            // quảnLýMượnToolStripMenuItem
+            // 
+            this.quảnLýMượnToolStripMenuItem.Name = "quảnLýMượnToolStripMenuItem";
+            this.quảnLýMượnToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.quảnLýMượnToolStripMenuItem.Text = "Quản Lý Mượn";
+            this.quảnLýMượnToolStripMenuItem.Click += new System.EventHandler(this.quảnLýMượnToolStripMenuItem_Click);
+            // 
+            // quảnLýTrảToolStripMenuItem
+            // 
+            this.quảnLýTrảToolStripMenuItem.Name = "quảnLýTrảToolStripMenuItem";
+            this.quảnLýTrảToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.quảnLýTrảToolStripMenuItem.Text = "Quản Lý Trả";
+            this.quảnLýTrảToolStripMenuItem.Click += new System.EventHandler(this.quảnLýTrảToolStripMenuItem_Click);
+            // 
+            // FormQLThuVien
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(953, 450);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "FormQLThuVien";
+            this.Text = "FormQLThuVien";
+            this.Load += new System.EventHandler(this.FormQLThuVien_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem hệThốngToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem quảnLýĐộcGiảToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem quảnLýSáchToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem quảnLýTácGiảToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem quảnLýMượnTrảToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem quảnLýMượnToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem quảnLýTrảToolStripMenuItem;
+    }
+}
+
---
+++ QuanLyThuVien/QuanLyThuVien/FormQLThuVien.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuanLyThuVien
+{
+    public partial class FormQLThuVien : Form
+    {
+        public FormQLThuVien()
+        {
+            InitializeComponent();
+        }
+
+        private void quảnLýSáchToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FormQLSach formQlSach = new FormQLSach();
+            formQlSach.Show();
+        }
+
+        private void quảnLýĐộcGiảToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FormQLDocGia formDocGia = new FormQLDocGia();
+            formDocGia.Show();
+        }
+
+        private void quảnLýTácGiảToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FormQLTacGia formQLTacGia = new FormQLTacGia();
+            formQLTacGia.Show();
+        }
+
+        private void hệThốngToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FormHeThong formHeThong = new FormHeThong();
+            formHeThong.Show();
+        }
+
+        
+
+        private void FormQLThuVien_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void quảnLýMượnToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FormQLMuon formQLMuon = new FormQLMuon();
+            formQLMuon.Show();
+        }
+
+        private void quảnLýTrảToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FormQLTra formQLTra = new FormQLTra();
+            formQLTra.Show();
+        }
+    }
+}
